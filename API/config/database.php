dbc93e5e7081c23e64300b70ba697fff82a566a1
AbdessamadPas <boughadoin@gmail.com>
2023-12-14 14:10:52 +0100

    deploy test

--- API/config/database.php
+++ API/config/database.php
@@ -46,11 +46,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('testdatabas.mysql.database.azure.com', '127.0.0.1'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'abdessamadpas'),
-            'username' => env('DB_USERNAME', 'abdessamadpas'),
-            'password' => env('DB_PASSWORD', 'Xwewe@1234'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -59,8 +59,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env(':\Users\LEGION\Desktop\SquaHR\API\DigiCertGlobalRootCA.crt.pem'),
-            ]) : [],
+                PDO::MYSQL_ATTR_SSL_CA => base_path('certificates/DigiCertGlobalRootCA.crt.pem'),
+                ]) : [],
         ],
 
         'pgsql' => [
